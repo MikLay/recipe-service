3e08cb7f5ee92130a5a6bc0a3edf8919b6aaee8d
Michael Fediuchenko <mixei112299@gmail.com>
2020-04-22 05:23:29 +0300

    Update Recipe with conditional rendering of auth user

--- client/src/scenes/Recipe/Recipe.js
+++ client/src/scenes/Recipe/Recipe.js
@@ -1,5 +1,5 @@
-import React, {useEffect, useState} from "react";
-import {useHistory, useLocation} from "react-router";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router";
 import api from "../../api/api";
 import styled from "styled-components";
 
@@ -20,48 +20,57 @@ const Img = styled.img`
 `;
 
 const Recipe = () => {
-    const [recipe, setRecipe] = useState(null);
-    const id = useLocation().pathname.substring("/recipes/".length);
-    const history = useHistory();
+  const [recipe, setRecipe] = useState(null);
+  const id = useLocation().pathname.substring("/recipes/".length);
+  const history = useHistory();
 
-    useEffect(() => {
-        api.fetchRecipe(id).then(setRecipe);
-    }, []);
+  useEffect(() => {
+    api.fetchRecipe(id).then(setRecipe);
+  }, []);
 
-    if (!recipe) {
-        return <h3>Loading...</h3>;
-    }
+  if (!recipe) {
+    return <h3>Loading...</h3>;
+  }
 
-    return (
-        <div className="container">
-            <Info>
-                <Header as="h1">{recipe.name}</Header>
-                <div className="d-flex m-3">
-                    <Img src={recipe.picture}/>
-                </div>
-                <div className="d-flex flex-column">
-                    <h3>{recipe.shortDesc}</h3>
-                    <p>{recipe.longDesc}</p>
-                </div>
-                <h6>{recipe.createDate}</h6>
-                <div className="d-flex m-4 btn-group-lg" style={{justifyContent: "center"}} role="group">
-                    <button className="btn btn-primary" onClick={() => history.push(`/edit/${id}`)}>
-                        Edit
-                    </button>
-                    <button
-                        className="btn btn-danger"
-                        color="red"
-                        onClick={() => {
-                            api.deleteRecipe(id);
-                            history.push("/recipes");
-                        }}
-                    >
-                        Delete
-                    </button>
-                </div>
-            </Info>
+  return (
+    <div className="container">
+      <Info>
+        <Header as="h1">{recipe.name}</Header>
+        <div className="d-flex m-3">
+          <Img src={recipe.picture} />
         </div>
-    );
+        <div className="d-flex flex-column">
+          <h3>{recipe.shortDesc}</h3>
+          <p>{recipe.longDesc}</p>
+        </div>
+        <h6>{recipe.createDate}</h6>
+        {localStorage.getItem("user_id") && (
+          <div
+            className="d-flex m-4 btn-group-lg"
+            style={{ justifyContent: "center" }}
+            role="group"
+          >
+            <button
+              className="btn btn-primary"
+              onClick={() => history.push(`/edit/${id}`)}
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-danger"
+              color="red"
+              onClick={() => {
+                api.deleteRecipe(id);
+                history.push("/recipes");
+              }}
+            >
+              Delete
+            </button>
+          </div>
+        )}
+      </Info>
+    </div>
+  );
 };
 
 export default Recipe;
